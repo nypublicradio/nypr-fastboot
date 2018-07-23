e7e83df89dc50f0c44b7785c323f95cb7f00af97
Brian Whitton <me@brianwhitton.com>
2018-07-23 16:23:10 -0400

    some changes to the server export
    pass in all args; don't rely on envvars
    accept options for fastboot server

--- index.js
+++ index.js
@@ -5,29 +5,33 @@ const S3Notifier = require('fastboot-s3-notifier');
 const healthChecker = require('./lib/health-checker-middleware');
 const preview = require('./lib/preview-middleware');
 
-require('dotenv').config();
+const FASTBOOT_DEFAULTS = {
+  gzip: true,
+  chunkedResponse: true,
+};
 
-let downloader = new S3Downloader({
-  bucket: process.env.AWS_BUCKET,
-  key: process.env.FASTBOOT_MANIFEST,
-});
+module.exports = function({ bucket, manifestKey, healthCheckerUA, fastbootConfig = {} }) {
 
-let notifier = new S3Notifier({
-  bucket: process.env.AWS_BUCKET,
-  key: process.env.FASTBOOT_MANIFEST,
-});
+  fastbootConfig = {...FASTBOOT_DEFAULTS, ...fastbootConfig};
 
-module.exports = function({ bucket, uaString }) {
+  let downloader = new S3Downloader({
+    bucket,
+    key: manifestKey,
+  });
+
+  let notifier = new S3Notifier({
+    bucket,
+    key: manifestKey,
+  });
 
   let server = new FastBootAppServer({
     beforeMiddleware(app) {
-      app.use(healthChecker({ uaString }));
+      app.use(healthChecker({ uaString: healthCheckerUA }));
       app.use(preview({ bucket }));
     },
     downloader,
     notifier,
-    gzip: true,
-    chunkedResponse: true,
+    ...fastbootConfig,
   });
 
   return server;
