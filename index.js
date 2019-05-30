064a67297c56066adc5f9caa41dc4caf24fa57b1
Brian Whitton <bwhitton@wnyc.org>
2019-05-30 00:42:10 -0400

    move logger into its own lib

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const FastBootAppServer = require('fastboot-app-server');
 const S3Downloader = require('fastboot-s3-downloader');
 const S3Notifier = require('fastboot-s3-notifier');
 const Sentry = require('@sentry/node');
-const morgan = require('morgan');
 
 const statsd = require('./lib/statsd-client-middleware')
 const healthChecker = require('./lib/health-checker-middleware');
 const preview = require('./lib/preview-middleware');
+const logger = require('./lib/logger-middleware');
 
 const FASTBOOT_DEFAULTS = {
   gzip: true,
@@ -27,23 +27,7 @@ module.exports = function({ bucket, manifestKey, healthCheckerUA, sentryDSN, fas
   let beforeMiddleware = app => {
     app.use(Sentry.Handlers.requestHandler());
     app.use(Sentry.Handlers.errorHandler());
-    /* The reason for the weird colon escaping here is the Morgan Library. 
-    The way it identifies special keywords is to preceed them with a ':', i.e. ':remote-user'
-    So if we want to include actual ':' in the string message, they must be escaped.*/
-    app.use(morgan('{\
-"@timestamp"\: ":date[clf]",\
-"message"\: "\
-clientip\::req[x-forwarded-for]|\
-user\::remote-user|\
-verb\::method|\
-request\::url|\
-protocol\::http-version|\
-status\::status|\
-size\::res[content-length]|\
-referrer\:":referrer"|\
-agent\:":user-agent"|\
-duration\::response-time"}',
-        { skip: req => req.headers['user-agent'] == 'ELB-HealthChecker/2.0' }));
+    app.use(logger());
     app.use(statsd({host: 'graphite.nypr.digital', namespace: `${env}.${serviceName}`}));
     app.use(healthChecker({ uaString: healthCheckerUA }));
     app.use(preview({ bucket }));
