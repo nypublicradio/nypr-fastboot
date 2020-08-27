85bb982562679c44290c954b993d609a5a9d5714
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2020-08-26 21:36:49 -0400

    fix typo

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ module.exports = function({ bucket, manifestKey, healthCheckerUA, sentryDSN, log
     app.use(preview({ bucket }));
 
     if (fastbootConfig.distPath) {
-      // if distPath isn't set, we're running locally
+      // if distPath is set, we're running locally
       const path = require('path');
       const express = require('express');
       let assetPath = path.join(fastbootConfig.distPath, 'assets');
